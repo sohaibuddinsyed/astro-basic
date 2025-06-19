31adbe722c03689bdc11c163456a13feebe2570c
Sohaib Uddin Syed <sohaibus@amazon.com>
2025-06-19 15:01:00 -0700

    add ssr page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import amplify from '@craftlions/astro-adapter'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    adapter: amplify()
+});
